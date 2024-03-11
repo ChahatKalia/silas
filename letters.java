8929a4cd1af77095a154634c342b56709bc45235
Chahat Kalia <chachaatcha@gmail.com>
2024-03-10 22:32:52 -0400

    Update letters.java

--- letters.java
+++ letters.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class letters {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word;
+        char letter;
+        int[] cool = new int[26];
         System.out.println("Hii");
+        System.out.println("what's your word?");
+        word = scanner.nextLine();
+        for ( int i = 0; i<word.length(); i++) {
+            letter = word.charAt(i);
+            
+        }
+        System.out.println("Letter counts are:");
+        for (int i: cool )ß
+
+
     }
 }
