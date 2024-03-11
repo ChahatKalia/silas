73b5fff439a3c818b33f8411862f0957e36f573a
SiPi1 <silas.wickenden@gmail.com>
2024-03-10 22:42:54 -0400

    New commit
    Written comments

--- letters.java
+++ letters.java
@@ -5,17 +5,22 @@ public class letters {
         Scanner scanner = new Scanner(System.in);
         String word;
         char letter;
-        int[] cool = new int[26];
-        System.out.println("Hii");
+        int[] letters = new int[26];
+        //for each int in letters
+            //make it 0
+
         System.out.println("what's your word?");
         word = scanner.nextLine();
+
         for ( int i = 0; i<word.length(); i++) {
             letter = word.charAt(i);
-            
+            //letters[index of ^that]++;
         }
+
         System.out.println("Letter counts are:");
-        for (int i: cool )ß
-
-
+        for (int i: letters ) {
+            //check if there are letters (i == 0)
+            //print out the count if there are
+        }
     }
 }
